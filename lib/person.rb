84822273f28b17f1066831709241f06b6070f3b2
Tatiana <Bowmantatiana2@gmail.com>
2023-01-31 15:01:13 -0800

    commit tree 0973b05c0ca967cda091af3bf8023ed720f97f70

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,96 @@
-# your code goes here
+require "pry"
+class Person
+    attr_accessor(:bank_account)
+    attr_reader(:name, :happiness, :hygiene)
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+def name=(name)
+   @name = name
+end
+
+    def happiness=(points)
+        if(points >= 0 && points <= 10)
+            @happiness = points
+            elsif(points > 10)
+                @happiness = 10
+                elsif(points < 0)
+                    @happiness = 0
+        end
+
+    end
+
+    def hygiene=(points)
+        if(points >= 0 && points <= 10)
+            @hygiene = points
+            elsif(points > 10)
+                @hygiene = 10
+                elsif(points < 0)
+                    @hygiene = 0
+        end
+    end
+
+    #non attribute methods
+
+    def happy?
+        if(@happiness > 7)
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if(@hygiene > 7)
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if(topic == "politics")
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+            elsif(topic === "weather")
+                self.happiness += 1
+                friend.happiness += 1
+                "blah blah sun blah rain"
+            else
+                "blah blah blah blah blah"
+            end
+    end
+end
+
+
+p1 = Person.new("Tatiana",1000000, 10, 10)
+p2 = Person.new("Lucas", 1000000, 10, 9)
+
